1e899ea92ae35b57504239bac81584c7d940b925
task11 <task11@naver.com>
2021-11-19 17:55:48 +0900

    Request JSON & View Data

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ function App() {
     fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
       .then((response) => response.json())
       .then((json) => {
-        setMovie(json);
+        setMovie(json.data.movies);
         setLoading(false);
       });
-
   }, []);
 
   return (
     <div>
       {loading ? <strong>Loading ...</strong> : null}
+      <h1>Movie List</h1>
+      <ul>
+        {movie.map((item, id) => {
+          return <li>제목 : {item.title} 평점 : {item.rating} </li>;
+        })}
+      </ul>
     </div>
   );
 }
