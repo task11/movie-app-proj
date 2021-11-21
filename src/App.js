25b8e9316a8b3bbf82f4775fc6b3ca60f3965d0a
task11 <task11@naver.com>
2021-11-21 19:02:56 +0900

    import Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
-  return <Home />;
+  return <Router>
+    <Routes>
+      <Route
+        path="/movie"
+        element={<Detail />}
+      />
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+    </Routes>
+  </Router>;
 }
 
 export default App;
