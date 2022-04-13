f07762a17220056af6502d62f214db228bc4a738
task11 <task11@naver.com>
2022-04-13 21:59:18 +0900

    style:  Movie detail

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 
-function MovieDetail({ title, year, genres, description, date, coverImg }) {
+function MovieDetail({ title, year, genres, description, coverImg }) {
   return (
     <div>
+      <img src={coverImg} alt="cover image"></img>
       <h3> <Link to="/movie-app-proj">back</Link></h3>
       <div>{title}</div>
       <span>{year}</span>
-      <span>{date}</span>
       <div>{genres}</div>
       <p>{description}</p>
-      <img src={coverImg} alt="cover image"></img>
     </div>
   );
 
