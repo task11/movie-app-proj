a7c2a08d3ff33dfb52186990dce65f638bcfca59
task11 <task11@naver.com>
2022-04-16 23:35:41 +0900

    style: Apply styled-component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,33 @@ import {
 } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white
+`;
 
 function App() {
-  return <Router>
+  return <Father>
+    <Box bgColor="teal">
+      <Text>Text</Text>
+    </Box>
+    <Box bgColor="tomato"></Box>
+  </Father>;
+}
+
+/* 
+<Router>
     <Routes>
       <Route
         path="/movie/:id"
@@ -19,7 +43,6 @@ function App() {
       />
 
     </Routes>
-  </Router>;
-}
+  </Router>; */
 
 export default App;
